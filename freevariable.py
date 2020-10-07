ef0162e258a85a8ae2e8fb39692cab3c64dfae02
Kevin Koch <cptwalmart@gmail.com>
2020-10-07 13:47:24 -0400

    Started working on Nullspace. Added a Pivot and Free Variable function to Nyauki.py

---
+++ freevariable.py
@@ -0,0 +1,19 @@
+import Nayuki
+
+F = Nayuki.PrimeField(5)
+B = Nayuki.Matrix(3, 3, F)
+cols = 3
+rows = 3
+data = [1, 2 ,3, 2, 4, 0, 1, 2 ,3]
+inc = 0
+for i in range(rows):  # For each column
+    for j in range(cols):
+        B.set(i,j,data[inc])
+        inc += 1
+print(B)
+B.reduced_row_echelon_form()
+print(B)
+test = B.find_pivots()
+print(test)
+test = B.find_free_variables()
+print(test)
