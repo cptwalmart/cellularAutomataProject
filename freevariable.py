d4e4f0a7651600cd40df5a8f390b65ccd138b995
Hyper <56860100+Hypertension1999@users.noreply.github.com>
2020-11-23 15:40:41 -0500

    Add files via upload

--- freevariable.py
+++ freevariable.py
@@ -61,7 +61,7 @@ data = [0, 1, 1, 0, 0, 0, 0, 0, 1,
 
 #######################################     END     #######################################
 
-####################################### 8 x 8 mod(3) #######################################
+""" ####################################### 8 x 8 mod(3) #######################################
 alphabet = 3 # our mod(p)
 size = 8
 F = Nayuki.PrimeField(alphabet)
@@ -78,13 +78,74 @@ data = [1, 1, 1, 0, 0, 0, 0, 0,
         1, 1, 0, 0, 0, 0, 0, 1]
 
 
+#######################################     END     ####################################### """
+
+
+
+
+
+
+
+
+
+
+
+
+""" ####################################### 5 x 5 mod(5) #######################################
+alphabet = 5 # our mod(p)
+size = 5
+F = Nayuki.PrimeField(alphabet)
+B = Nayuki.Matrix(size, size, F)
+rows = cols = size
+
+data = [0, 1, 0, 0, 1,
+        1, 0, 1, 0, 0,
+        0, 1, 0, 1, 0,
+        0, 0, 1, 0, 1,
+        1, 0, 0, 1, 0]
+
+#######################################     END     ####################################### """
+
+""" ####################################### 9 x 9 mod(3) #######################################
+alphabet = 3 # our mod(p)
+F = Nayuki.PrimeField(alphabet)
+B = Nayuki.Matrix(9, 9, F)
+size = 9
+rows = cols = size
+data = [1, 1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 1, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 1, 1, 1, 0, 0,
+        0, 0, 0, 0, 0, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 1]
+
+#######################################     END     ####################################### """
+
+####################################### 7 x 7 mod(3) #######################################
+alphabet = 3 # our mod(p)
+size = 7
+F = Nayuki.PrimeField(alphabet)
+B = Nayuki.Matrix(size, size, F)
+rows = cols = size
+
+data = [0, 1, 0, 0, 0, 1, 1,
+        1, 0, 1, 0, 0, 0, 1,
+        1, 1, 0, 1, 0, 0, 0,
+        0, 1, 1, 0, 1, 0, 0,
+        0, 0, 1, 1, 0, 1, 0,
+        1, 0, 0, 1, 1, 0, 1,
+        1, 1, 0, 0, 1, 1, 0]
+
 #######################################     END     #######################################
 
 ############################# Find T^k = T^n-k (IF there is one) ##########################
 # finds the max number of cycle of transtion matrix to compute
 def detect_cycle_transtion(transition, alphabet):
 
-    u_bound = 10000
+    u_bound = 100000
     power = 1
     M_list = []
 
@@ -186,35 +247,35 @@ Basis = B.get_nullspace()
 print(Basis)
 
 #######################################     END     #######################################
-
+""" 
 power = 1
 for i in range(n):
     print("\n(T)^{}: ".format(power))
     result_matrix = (np.linalg.matrix_power(transition, power)) % alphabet
     print(result_matrix)
     power += 1
-
+ """
 ####################################### NULL POW(N) #######################################
 
 Nullspace_list = { "nullspace": [], "power": []}
 power = 1
 for i in range(n):
-    print("\n(T)^{} - I: ".format(power))
+    #print("\n(T)^{} - I: ".format(power))
     result_matrix = (np.linalg.matrix_power(transition, power) - I) % alphabet
-    print(result_matrix)
+    #print(result_matrix)
 
     # Set Nayuki Matrix to reult of (T)^n - I
     for i in range(size):  # For each column
         for j in range(size):
             B.set(i,j,int(result_matrix[i,j]))
 
-    print("\nrref for (T)^{} - I: ".format(power))
+    #print("\nrref for (T)^{} - I: ".format(power))
     B.reduced_row_echelon_form()
-    print(B)
+    #print(B)
 
-    print("\nnullspace for (T)^{} - I: ".format(power))
+    #print("\nnullspace for (T)^{} - I: ".format(power))
     Basis = B.get_nullspace()
-    print(Basis)
+    #print(Basis)
     
     Nullspace_list["nullspace"].append(Basis)
     Nullspace_list["power"].append(power)
