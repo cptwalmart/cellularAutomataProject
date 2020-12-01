6091202dfacdd05dd9a938af06c8d5608f4101a1
Malik Green <green.malik5@gmail.com>
2020-11-30 22:22:52 -0500

    Update ca_modules_installer.py
    No need for text file with modules. just added them all on one line. 
    (Assumes pip is intstalled)

--- ca_modules_installer.py
+++ ca_modules_installer.py
@@ -1,10 +1,10 @@
 """ Requirements for this installer to work properly. """
-# Must have the "neededModules.txt" file in same directory/folder in order to install.
 # Must have pip installer installed on computer.
 import os
 try:
-    os.system('pip install -r ca_needed_modules.txt')
+    os.system('pip install numpy==1.8.0rcl matplotlib==1.3.1 sympy PyQt5')
 except:
     print("There was a problem installing the proper modules.\nMake sure 'ca_needed_modules.txt' is in the same directory as this python script is.\n")
 else:
     print("All of the needed modules for the cellular automata app were installed properly.\n")
+
