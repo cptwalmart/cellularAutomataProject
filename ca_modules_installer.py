c6ffa6295c1286715e7a810b571c4247a50d150d
Malik Green <green.malik5@gmail.com>
2020-12-02 13:08:55 -0500

    Update ca_modules_installer.py

--- ca_modules_installer.py
+++ ca_modules_installer.py
@@ -1,6 +1,6 @@
 """ Requirements for this installer to work properly. """
 # Must have pip installer installed on computer.
-# Will check if pip is installed on computer; if not, this script will install pip.
+# Will check if pip is installed on computer; if not, this script will install pip (for windows).
 
 import os
 from sys import platform
@@ -26,6 +26,7 @@ elif platform == "win32": # Windows.
     try:
         os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
         os.system('python3 get-pip.py')
+        os.system('python3 -m pip install --upgrade pip3')
         os.system('python3 -m pip3 install numpy matplotlib sympy PyQt5')
     except:
         print("There was a problem installing the proper modules.\nMake sure 'ca_needed_modules.txt' is in the same directory as this python script is.\n")
