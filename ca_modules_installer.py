07133b440207f2fdbd1727d52fea953350e1a308
Malik Green <green.malik5@gmail.com>
2020-12-02 15:03:52 -0500

    Update ca_modules_installer.py

--- ca_modules_installer.py
+++ ca_modules_installer.py
@@ -7,7 +7,7 @@ from sys import platform
 
 if platform == "linux" or platform == "linux2": # linux
     try:
-        os.system('python3 -m pip3 install numpy matplotlib sympy PyQt5')
+        os.system('python3 -m pip3 install numpy==1.19 matplotlib sympy PyQt5')
     except:
         print("There was a problem installing the proper modules.\nMake sure 'ca_needed_modules.txt' is in the same directory as this python script is.\n")
     else:
@@ -15,8 +15,7 @@ if platform == "linux" or platform == "linux2": # linux
 
 elif platform == "darwin": # OS X
     try:
-        os.system('python3 -m pip3 install numpy matplotlib sympy PyQt5')
-        #os.system('python3 -m pip install PyQt5')
+        os.system('python3 -m pip3 install numpy==1.19 matplotlib sympy PyQt5')
     except:
         print("There was a problem installing the proper modules.\nMake sure 'ca_needed_modules.txt' is in the same directory as this python script is.\n")
     else:
@@ -24,10 +23,7 @@ elif platform == "darwin": # OS X
 
 elif platform == "win32": # Windows.
     try:
-        os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-        os.system('python3 get-pip.py')
-        os.system('python3 -m pip install --upgrade pip3')
-        os.system('python3 -m pip3 install numpy matplotlib sympy PyQt5')
+        os.system('pip3 install numpy==1.19 matplotlib sympy PyQt5'')
     except:
         print("There was a problem installing the proper modules.\nMake sure 'ca_needed_modules.txt' is in the same directory as this python script is.\n")
     else:
